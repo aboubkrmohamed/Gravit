35d70a36eda08e542ca89c85c55f16603fef6e0c
aboubkrmohamed <aboubkrmohamed86@gmail.com>
2023-06-14 13:59:47 +0200

    update

--- js/js.js
+++ js/js.js
@@ -46,10 +46,11 @@
     });
 
 
- 
-// $('.autoplay').slick({
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-//     autoplay: true,
-//     autoplaySpeed: 200,
-//   });
+    $('.autoplay').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+      });
+
+      

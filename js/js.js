8aba55ebd85329b34a9fcffbebc7bb2c1ed33526
aboubkrmohamed <aboubkrmohamed86@gmail.com>
2023-06-16 16:01:29 +0200

    responsive2

--- js/js.js
+++ js/js.js
@@ -54,3 +54,23 @@
       });
 
       
+    //   navbar in small screen 
+    let show = document.getElementById("show");
+let menuecontent = document.getElementById("menuecontent");
+let close = document.getElementById("close");
+
+
+
+show.addEventListener("click", showside)
+
+function showside() {
+    menuecontent.style.left = "0";
+ }
+
+
+
+close.addEventListener("click", closeSlide)
+function closeSlide() {
+    menuecontent.style.left = "100%";
+
+}
